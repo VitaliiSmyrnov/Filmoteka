9d264443e1b397f3a2bc5428a84e701de1cb04c8
Katya-Kondratchuk <ekaterina.kondratchuk@gmail.com>
2022-11-24 22:20:24 +0200

    create

--- src/scripts/gallery.js
+++ src/scripts/gallery.js
@@ -1,14 +1,5 @@
-import { fetchGenre, fetchRandomFilm, fetchSearchFilm } from './fetch';
-import { formRef, galleryRef } from './refs';
+import { fetchGenre, fetchRandomFilm } from './fetch';
 import { renderFilmList } from './renderFilmList';
-
-function findGenres(filmId) {
-  return fetchGenre().then(({ genres }) =>
-    genres.filter(({ id, name }) => {
-      if (id === filmId) name;
-    })
-  );
-}
 
 function markupRandomFilms() {
   fetchRandomFilm().then(({ results }) =>
@@ -28,25 +19,10 @@ function markupRandomFilms() {
 
 markupRandomFilms();
 
-function handleSearchFormSubmit(e) {
-  e.preventDefault();
-  const query = e.target.elements['search-input'].value.trim();
-  galleryRef.innerHTML = '';
-
-  fetchSearchFilm(query).then(({ results }) =>
-    results.map(
-      ({ poster_path, original_title, title, genre_ids, release_date }) => {
-        return renderFilmList(
-          poster_path,
-          original_title,
-          title,
-          genre_ids,
-          release_date,
-          findGenres
-        );
-      }
-    )
+export function findGenres(filmId) {
+  return fetchGenre().then(({ genres }) =>
+    genres.filter(({ id, name }) => {
+      if (id === filmId) name;
+    })
   );
 }
-
-formRef.addEventListener('submit', handleSearchFormSubmit);
