d6503370e6231f56d8a0dae9c3a71f220efbd1b5
Katya-Kondratchuk <ekaterina.kondratchuk@gmail.com>
2022-11-27 00:31:19 +0200

    ccccccc

--- src/scripts/refs.js
+++ src/scripts/refs.js
@@ -5,4 +5,24 @@ const openModalBtn = document.querySelector('[data-modal-open]');
 const closeModalBtn = document.querySelector('[data-modal-close]');
 const modal = document.querySelector('[data-modal]');
 
-export { galleryRef, formRef, openModalBtn, closeModalBtn, modal };
+const goTop = document.getElementById('go-top');
+const headerContainer = document.querySelector('.header__container');
+
+const footerModal = document.querySelector('.footer__modal');
+const openTeamModal = document.querySelector('.footer-team__name');
+const closeTeamModal = document.querySelector('#teammates');
+const backdrop = document.querySelector('.backdrop');
+
+export {
+  galleryRef,
+  formRef,
+  openModalBtn,
+  closeModalBtn,
+  modal,
+  goTop,
+  headerContainer,
+  footerModal,
+  openTeamModal,
+  closeTeamModal,
+  backdrop,
+};
