bb1aa71cb29c8152c12e1d828eb1d5bea7506b5e
Paul Litinskiy <paul.litinskiy@gmail.com>
2022-11-28 00:09:29 +0100

    xxx

--- src/js/theme-change.js
+++ src/js/theme-change.js
@@ -1,22 +1,3 @@
-<<<<<<< HEAD
-const bodyRef = document.querySelector('body');
-const toggleRef = document.querySelector('#theme-switch-toggle');
-const footerDarktheme = document.querySelector('.footer');
-
-toggleRef.addEventListener('change', event => {
-  if (bodyRef.classList.contains('dark-theme')) {
-    bodyRef.classList.remove('dark-theme');
-    bodyRef.classList.add('light-theme');
-    footerDarktheme.classList.remove('dark-theme');
-  } else {
-    bodyRef.classList.remove('light-theme');
-    bodyRef.classList.add('dark-theme');
-    footerDarktheme.classList.add('dark-theme');
-  }
-});
-
-export { bodyRef, toggleRef, footerDarktheme };
-=======
 // import { setToLocalStorage, getFromLocalStorage } from "./local-storage_theme-switcher";
 
 // const clickBox = document.querySelector('#theme-switch-toggle');
@@ -80,4 +61,3 @@ const localStorageTheme = () => { //если тема в localStorage dark, то
 }
 inputChangeTheme.addEventListener("change", handleInputChange); //вешаем слушателя событий на переключатель темы
 localStorageTheme()
->>>>>>> dev
