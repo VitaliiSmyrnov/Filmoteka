eebc5d6ab495beb0636709dc134837a1b5bd81df
Paul Litinskiy <paul.litinskiy@gmail.com>
2022-11-29 15:11:24 +0100

    29_11 middle-day

--- src/scripts/pagination.js
+++ src/scripts/pagination.js
@@ -13,6 +13,22 @@ const instance = new Pagination(container, {
   visiblePages: 5,
   centerAlign: true,
   page: 1,
+   template: {
+    page: '<a href="#" class="tui-page-btn">{{page}}</a>',
+    currentPage: '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
+    moveButton:
+      '<a href="#" class="tui-page-btn tui-{{type}}">' +
+        '<span class="tui-ico-{{type}}">{{type}}</span>' +
+      '</a>',
+    disabledMoveButton:
+      '<span class="tui-page-btn tui-is-disabled tui-{{type}}">' +
+        '<span class="tui-ico-{{type}}">{{type}}</span>' +
+      '</span>',
+    moreButton:
+      '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' +
+        '<span class="tui-ico-ellip dots">...</span>' +
+      '</a>'
+  }
 });
 
 instance.on('afterMove', event => {
