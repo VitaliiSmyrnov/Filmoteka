7f2ec9e91ab29ce45a5ce0447e3ba70fdad79fa1
Borys Supreme <bvs.bromes@gmail.com>
2022-11-30 02:49:57 +0200

    markup iframe

--- src/scripts/refs.js
+++ src/scripts/refs.js
@@ -1,8 +1,6 @@
 const galleryRef = document.querySelector('.gallery');
 const formRef = document.querySelector('.form');
 const containerElem = document.querySelector('.modal__wrapper');
-// const addWatchBtn = document.querySelector('.film__button-add-to-watch');
-// const addQueueBtn = document.querySelector('.film__button-add-to-queue');
 
 const openModalBtn = document.querySelector('[data-modal-open]');
 const closeModalBtn = document.querySelector('[data-modal-close]');
