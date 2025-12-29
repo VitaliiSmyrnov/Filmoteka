acc8bba1b3add3600cac7da856073604849aacdd
VitaliiSmyrnov <vitaliy.smirnov@ukr.net>
2025-12-29 15:20:28 +0200

    Hide API key using environment variables

--- src/scripts/api.js
+++ src/scripts/api.js
@@ -1,5 +1,5 @@
 const BASE_URL = 'https://api.themoviedb.org/3';
-const API_KEY = '14f89e95e2687deafc45dced0859c8d0';
+const API_KEY = process.env.TMDB_API_KEY;
 const discover_point = 'discover/movie';
 const genre_point = `genre/movie/list`;
 const search_point = `search/movie`;
